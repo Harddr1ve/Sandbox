b73cbcf3c62463686a25785ef14a4ca8ade438c6
Harddrive <kolchinalexandr@mail.com>
2021-10-16 19:43:08 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//#include "gtest/gtest.h"
+
+
+bool isEqual(int val);
+
+int main() {
+
+//    testing::InitGoogleTest();
+//    std::cout << RUN_ALL_TESTS() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << isEqual(15);
+
+    return 0;
+}
+
+bool isEqual(int val)
+{
+    return !(val & 1);
+}
+
+//TEST(MainTest, EqualTest) {
+//    EXPECT_PRED1(isEqual, 10);
+//    EXPECT_EQ(isEqual(15), false);
+//}
