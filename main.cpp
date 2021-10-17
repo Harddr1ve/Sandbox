bea50fa78b73230a02f67a6713bfee20d02b9e06
Harddrive <kolchinalexandr@mail.com>
2021-10-17 03:30:21 +0300

    Started new task, build artifacts

--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include <forward_list>
+#include <algorithm>
 
+using namespace std;
 
-bool isEqual(int val);
+struct mem_block_t {
+    mem_block_t *next_mb; // next memory block or null
+    size_t mb_size; // memory block size
+    uint8_t buf[1]; // memory buffer of variable size
+};
+
+struct mem_file_img_t {
+    mem_file_img_t *next_mfi; // next memory image file or null
+    char path[PATH_MAX]; // file path
+};
 
 int main() {
+    int mem_block_count;
+    forward_list<mem_block_t> mem_block_list;
+    forward_list<mem_file_img_t> mem_file_list;
 
-    testing::InitGoogleTest();
-    std::cout << RUN_ALL_TESTS() << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-
-    std::cout << isEqual(15);
+    cout << "Enter mem block count: ";
+    cin >> mem_block_count;
 
     return 0;
-}
-
-bool isEqual(int val)
-{
-    return !(val & 1);
-}
-
-TEST(MainTest, EqualTest) {
-    EXPECT_PRED1(isEqual, 10);
-    EXPECT_EQ(isEqual(15), false);
 }
